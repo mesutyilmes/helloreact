85a6b3eb77d1952b6ec50d4da8265d6f89601382
Mesut Yilmaz <mesutyilmes@gmail.com>
2019-04-06 23:45:11 +0300

    user arayuz olusturma ve font awesome

--- src/components/User.js
+++ src/components/User.js
@@ -5,16 +5,19 @@ import PropTypes from 'prop-types'
  class User extends Component {
   render() {
     //Destructing
-    const {name} = this.props
+    const {name,department,salary} = this.props
     return (
-      <div>
-        <ul>
-            <li>Name : {name}</li>
-            <li>Department : {this.props.department}</li>
-            <li>Salary : {this.props.salary}</li>
-        </ul>
-        <hr/>
-      </div>
+        <div className="col-md-8 mb-4">
+            <div className="card-header d-flex justify-content-between">
+                <h4 className="d-inline">{name}</h4>
+                <i className="far fa-trash-alt" style={{cursor:"pointer"}}></i>
+            </div>
+            <div className="card-body">
+                <p className="card-text">Maas : {salary}</p>
+                <p className="card-text">Department : {department}</p>
+            </div>
+        </div>
+     
     )
   }
 }
